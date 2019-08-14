8cc941ca0087a8e2766a888dd105a91ada1a4ecd
@snowman <jimmoffitt@yahoo.com>
2019-08-14 13:14:35 -0600

    Update tweet-nasa-photo.py

--- tweet-nasa-photo.py
+++ tweet-nasa-photo.py
@@ -2,6 +2,9 @@ import sys
 import requests
 import tweepy
 import shutil
+import os
+from dotenv import load_dotenv
+load_dotenv(verbose=True)  # Throws error if it can't find .env file
 
 # Twitter API details. Consumer keys and access tokens, used for OAuth.
 consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
@@ -11,13 +14,15 @@ access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 
 # NASA API details
 nasa_key = os.getenv("NASA_KEY")
-nasa_api = os.getenv("NASA_API"
+nasa_api = os.getenv("NASA_API")
+nasa_url = f"{nasa_api}?api_key={nasa_key}"
+print (nasa_url)
 
 #Bot details
 photo_filename = os.getenv("PHOTO_FILENAME")
 
-
-tweet_message = ' ✨ NASA Astronomy Picture of the Day from a 40-line Python script. ✨'
+#Change this to make your Tweets more interesting...
+tweet_message = ' ✨ NASA Astronomy Picture of the Day from a 40-line Python script. #TapIntoTwitter ✨'
 
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -27,16 +32,22 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # Call NASA API
-r = requests.get(url = nasa_api)
+print ("Calling the NASA API")
+r = requests.get(url = nasa_url)
 data = r.json()
+print (data)
+
+#Extract response metadata for downloading media and including in Tweet message. 
 photo_url = data['url']
 
 # Download file
+print ("Downloading a single NASA photo...")
 response = requests.get(photo_url, stream=True)
 with open(photo_filename, 'wb') as out_file:
     shutil.copyfileobj(response.raw, out_file)
 del response
 
+#Prepare post metdata, not that this script only handles a single photo. 
 filenames = [photo_filename] # 
 media_ids = []
 for filename in filenames:

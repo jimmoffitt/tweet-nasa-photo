9e8de19ba1a4fcd63b22c932aff0ed261b5d2e2d
@snowman <jimmoffitt@yahoo.com>
2019-08-14 11:29:25 -0600

    Update tweet-nasa-photo.py

--- tweet-nasa-photo.py
+++ tweet-nasa-photo.py
@@ -9,6 +9,9 @@ consumer_secret =  os.getenv("consumer_secret")
 access_token = ''
 access_token_secret = ''
 nasa_key = ''
+nasa_api = f"https://api.nasa.gov/planetary/apod?api_key={nasa_key}"
+photo_filename = 'nasa_apod.jpg'
+tweet_message = 'NASA Astrono ✨'
 
 
 # OAuth process, using the keys and tokens
@@ -18,9 +21,8 @@ auth.set_access_token(access_token, access_token_secret)
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
 
-# Get NASA media
-apod_url = f"https://api.nasa.gov/planetary/apod?api_key={nasa_key}"
-r = requests.get(url = apod_url)
+# Call NASA API
+r = requests.get(url = nasa_api)
 data = r.json()
 photo_url = data['url']
 
@@ -30,15 +32,11 @@ with open('nasa_apod.jpg', 'wb') as out_file:
     shutil.copyfileobj(response.raw, out_file)
 del response
 
-filenames = ['nasa_apod.jpg']
+filenames = [photo_filename] # 
 media_ids = []
 for filename in filenames:
      res = api.media_upload(filename)
      media_ids.append(res.media_id)
 
-tweet_text = 'Tweeting with Python ✨'
-
-print(media_ids)
-
 # tweet with multiple images
-api.update_status(status=tweet_text, media_ids=media_ids)
+api.update_status(status=tweet_message, media_ids=media_ids)

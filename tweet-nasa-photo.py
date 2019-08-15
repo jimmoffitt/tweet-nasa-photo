300af2569b4227b3371938e8b496e9a7153bdd38
@snowman <jimmoffitt@yahoo.com>
2019-08-15 09:47:12 -0600

    Update tweet-nasa-photo.py

--- tweet-nasa-photo.py
+++ tweet-nasa-photo.py
@@ -6,6 +6,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv(verbose=True)  # Throws error if it can't find .env file
 
+print ("Retrieving credentials...")
 # Twitter API details. Consumer keys and access tokens, used for OAuth.
 consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
 consumer_secret =  os.getenv("TWITTER_CONSUMER_SECRET")
@@ -16,13 +17,10 @@ access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 nasa_key = os.getenv("NASA_KEY")
 nasa_api = os.getenv("NASA_API")
 nasa_url = f"{nasa_api}?api_key={nasa_key}"
-print (nasa_url)
 
 #Bot details
 photo_filename = os.getenv("PHOTO_FILENAME")
-
-#Change this to make your Tweets more interesting...
-tweet_message = ' ✨ NASA Astronomy Picture of the Day from a 40-line Python script. #TapIntoTwitter ✨'
+tweet_message = ' ✨ NASA Astronomy Picture of the Day from an example Python script.  #TapIntoTwitter ✨'
 
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -32,28 +30,31 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # Call NASA API
-print ("Calling the NASA API")
+print ('Calling the NASA API and retrieving photo metadata...')
 r = requests.get(url = nasa_url)
 data = r.json()
-print (data)
+#print (data)
 
-#Extract response metadata for downloading media and including in Tweet message. 
 photo_url = data['url']
+photo_title = data['title']
+#photo_description = data['explanation'] #Typically more than 280 characters.
+#Update tweet_message with this metadata
+tweet_message = f"{photo_title} \n \n {tweet_message}"
 
 # Download file
-print ("Downloading a single NASA photo...")
+print ('Downloading the NASA media...')
 response = requests.get(photo_url, stream=True)
 with open(photo_filename, 'wb') as out_file:
     shutil.copyfileobj(response.raw, out_file)
 del response
 
-#Prepare post metdata, not that this script only handles a single photo. 
-filenames = [photo_filename] # 
+filenames = [photo_filename] 
 media_ids = []
 for filename in filenames:
      res = api.media_upload(filename)
      media_ids.append(res.media_id)
 
 # tweet with multiple images
-print (f"Tweeting: {tweet_message}"
+print ('Posting Tweet with media...')
 api.update_status(status=tweet_message, media_ids=media_ids)
+

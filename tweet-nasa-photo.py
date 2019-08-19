b7ed5c91cd6dac890e1d8f33b2c7426e64125536
@snowman <jimmoffitt@yahoo.com>
2019-08-19 13:07:44 -0600

    Update tweet-nasa-photo.py

--- tweet-nasa-photo.py
+++ tweet-nasa-photo.py
@@ -19,8 +19,8 @@ nasa_api = os.getenv("NASA_API")
 nasa_url = f"{nasa_api}?api_key={nasa_key}"
 
 #Bot details
-photo_filename = os.getenv("PHOTO_FILENAME")
-tweet_message = ' ✨ NASA Astronomy Picture of the Day from an example Python script.  #TapIntoTwitter ✨'
+media_filename = os.getenv("PHOTO_FILENAME")
+tweet_message = ' ✨ Tweeting the NASA Astronomy Picture of the Day from an example Python script. #TapIntoTwitter ✨'
 
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -35,26 +35,42 @@ r = requests.get(url = nasa_url)
 data = r.json()
 #print (data)
 
-photo_url = data['url']
-photo_title = data['title']
-#photo_description = data['explanation'] #Typically more than 280 characters.
+media_url = data['url']
+media_title = data['title'] #Do videos have titles? Assume so ;)
+#media_description = data['explanation'] #Typically more than 280 characters. TODO: Build a mini-Tweet storm.
+
 #Update tweet_message with this metadata
-tweet_message = f"{photo_title} \n \n {tweet_message}"
+tweet_message = f"{media_title} \n \n {tweet_message}"
 
-# Download file
-print ('Downloading the NASA media...')
-response = requests.get(photo_url, stream=True)
-with open(photo_filename, 'wb') as out_file:
-    shutil.copyfileobj(response.raw, out_file)
-del response
+#Inspect URL and determine whether will need to download a photo file so we can natively Tweet it, OR
+# If it is a video, then just post a link to the video hosted elsewhere...
+native_media = False
+if media_url.endswith('jpg') or media_url.endswith('png'):
+    
+    native_media = True
+    # Download file
+    print ('Downloading the NASA media...')
+    response = requests.get(media_url, stream=True)
+    with open(media_filename, 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    del response
 
-filenames = [photo_filename] 
-media_ids = []
-for filename in filenames:
-     res = api.media_upload(filename)
-     media_ids.append(res.media_id)
+    filenames = [media_filename] 
+    media_ids = []
+    for filename in filenames:
+        res = api.media_upload(filename)
+        media_ids.append(res.media_id)
+else:
+    print ("It's movie time")
+    native_media = False
+    tweet_message = tweet_message + "#{media_url}"
 
-# tweet with multiple images
-print ('Posting Tweet with media...')
-api.update_status(status=tweet_message, media_ids=media_ids)
+if native_media:
+    print ('Posting Tweet with media...')
+    api.update_status(status=tweet_message, media_ids=media_ids)
+else:
+    tweet_text = tweet_message + " It's movie time... "
+    print ('Pain old Tweet with an URL in message...')
+    api.update_status(status=tweet_message)
+
 
